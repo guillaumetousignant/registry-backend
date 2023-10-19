ebb6e680960eecd1148f962036e1a18a3c4568fe
Guillaume Tousignant <moi_guillaume@hotmail.com>
2023-10-18 21:53:02 -0400

    Added rocket

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use std::error::Error;
+
+#[rocket::get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    let _rocket = rocket::build()
+        .mount("/", rocket::routes![index])
+        .launch()
+        .await?;
+
+    Ok(())
 }
