6c9a43fd06dad9ea0e2a08686713ae3e096b2f4f
Guillaume Tousignant <moi_guillaume@hotmail.com>
2024-01-18 18:32:42 -0500

    Clearer instructions for secret key

--- src/routes/authorization/authorization.rs
+++ src/routes/authorization/authorization.rs
@@ -1,6 +1,9 @@
 use rocket_jwt::jwt;
 
-static SECRET_KEY: &'static str = include_str!(env!("REGISTRYSECRETKEY_FILE", "specify the path to the file containing the secret key with the REGISTRYSECRETKEY_FILE environment variable"));
+static SECRET_KEY: &'static str = include_str!(env!(
+    "REGISTRYSECRETKEY_FILE",
+    "specify the path to the file containing the secret key"
+));
 #[jwt(SECRET_KEY, exp = 3600, leeway = 60)]
 pub struct UserClaim {
     pub id: String,
